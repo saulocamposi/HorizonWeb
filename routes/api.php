ba0e015f213200e26c2f55360ff99834f0d8dbd3
saulo <saulocamposi@gmail.com>
2023-11-14 04:32:28 +0000

    Add -- api routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CountryController;
+use App\Http\Controllers\NewsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/countries', [CountryController::class, 'getAllCountries']);
+Route::get('/country/{code}', [CountryController::class, 'getCountryDetails']);
+
+Route::get('/news/{countryCode}/{language}/{category}', [NewsController::class, 'getPaginatedNews']);
+Route::get('/news/{countryCode}/{page}', [NewsController::class, 'getPaginatedNews']);
+
+Route::post('/country/{countryCode}/{category}', [CountryController::class, 'addCategory']);
+Route::delete('/country/{countryCode}/{category}', [CountryController::class, 'removeCategory']);
